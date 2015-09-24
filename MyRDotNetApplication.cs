c9bb85ce80ae4516730d152ec1231a920f62dab8
Simon <simonmackinnon@users.noreply.github.com>
2015-09-24 10:01:22 +1000

    Update MyRDotNetApplication.cs

--- MyRDotNetApplication.cs
+++ MyRDotNetApplication.cs
@@ -6,29 +6,33 @@ namespace MyRDotNetApplication
 	{
 	        static void Main(string[] args)
 	        {
+	        	//Assume the file exists
 		    	string rawLatencyOutput = @"C:\\latencyOutput.csv";
 		
+			//Setup and init a new R-engine
 		    	REngine.SetEnvironmentVariables();
 		    	REngine engine = REngine.GetInstance();
 		    	engine.Initialize();
 		
+			//Call the function to evaluate the script
 		    	GenetateLatencyVsMessageLengthPlot_RScript(engine, rawLatencyOutput);
 		
+			//Stop the R-engine when done
 		    	engine.Dispose();
 	    	}
 			
 		static void GenetateLatencyVsMessageLengthPlot_RScript(REngine engine, string latencyCsvFilename)
 	        {
-			////////////////////////////////////////////////////////
-			//  R-Script to generate plot            
+	        	//For formatting purposes, make sure the filename is acceptable for R function read.csv
 			string fileToReadFromCommand = latencyCsvFilename.Replace(@"\", @"/");
 			
+			//Convert to R character vector
 			CharacterVector cvFilename = engine.CreateCharacterVector(new[] { fileToReadFromCommand });
-			
+			// and assign it to variable (in R engine) called fileToReadFrom
 			engine.SetSymbol("fileToReadFrom", cvFilename);
 			
-			engine.Evaluate(MyRDotNetApplication.Properties.Resources.latencyVsMessageLengthScatterPlot); //script is a string resource
-			//////////////////////////////////////////////////////            
+			//And then evaluate the script - this uses the 'fileToReadFrom' in a read.csv call
+			engine.Evaluate(MyRDotNetApplication.Properties.Resources.latencyVsMessageLengthScatterPlot); //R-Script to generate plot  
 	        }
 	}	
 }
